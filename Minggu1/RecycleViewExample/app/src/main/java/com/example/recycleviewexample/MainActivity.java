0b57c8b69081edcbc26d269b0af54e597e606b34
ResaWahyuSaputra <resawahyu.sdi24@gmail.com>
2021-04-17 20:05:10 +0700

    recycleview

---
+++ Minggu1/RecycleViewExample/app/src/main/java/com/example/recycleviewexample/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.recycleviewexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import java.util.ArrayList;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private MahasiswaAdapter adapter;
+    private ArrayList<Mahasiswa> mahasiswaArrayList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addData();
+        recyclerView = (RecyclerView) findViewById(R.id.recycle_view);
+        adapter = new MahasiswaAdapter(mahasiswaArrayList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    void addData() {
+        mahasiswaArrayList = new ArrayList<>();
+        mahasiswaArrayList.add(new Mahasiswa("Dicky Kurnia Ramadhan", "E41191553", "085156186156"));
+        mahasiswaArrayList.add(new Mahasiswa("Satsa Ratri Hastutik", "E41191795", "081339011430"));
+        mahasiswaArrayList.add(new Mahasiswa("Ulfiatun Hasanah", "E41191524", "085749253627"));
+        mahasiswaArrayList.add(new Mahasiswa("Ezra Alannathaniel", "E41191741", "085157815688"));
+        mahasiswaArrayList.add(new Mahasiswa("Naufal Akbar Kardieno Yasamita ", "E41191784", "082143404737"));
+        mahasiswaArrayList.add(new Mahasiswa("Nur Syahbana R", "E41191868", "0895366755164"));
+    }
+}
